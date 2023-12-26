8bb37f71ec6937d5e25790d927fcc87c85d315fc
Nathan Sjoquist <nathan.sjoquist2@kroger.com>
2023-12-26 08:18:51 -0500

    respond to OPTIONS with access control headers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,37 +8,59 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 import {getNearEarthObjectsData} from "./getNearEarthObjectsData";
 
-export const helloWorld = onRequest((request, response) => {
-    console.log(request);
-    logger.info("Hello logs!", {structuredData: true});
-    response.status(200);
-    response.json({
-        message: "Firebase!",
-    });
-});
+exports.getNearEarthObjects = onRequest((request, response) => {
+    // Set CORS headers
+    // Note: Replace "https://yourapp.domain" with the actual domain you want to allow.
+    response.set("Access-Control-Allow-Origin", "*");
 
-export const getNearEarthObjects = onRequest((request, response) => {
-    console.log("handling GET: " + request.url);
-    const dateString: string = request.query.date as unknown as string;
-    const searchDateNumber = Date.parse(dateString);
+    // Handles preflight requests for CORS
+    if (request.method === "OPTIONS") {
+        // Send response to OPTIONS requests
+        response.set("Access-Control-Allow-Methods", "GET, POST");
+        response.set("Access-Control-Allow-Headers", "Content-Type");
+        response.set("Access-Control-Max-Age", "3600");
+        response.status(204);
+    } else {
+        // Your function logic goes here
+        console.log("handling GET: " + request.url);
+        const dateString: string = request.query.date as unknown as string;
+        const searchDateNumber = Date.parse(dateString);
 
-    if (isNaN(searchDateNumber)) {
-        console.log("invalid date in query: " + dateString);
-        response
-            .status(404);
-    }
+        if (isNaN(searchDateNumber)) {
+            console.log("invalid date in query: " + dateString);
+            response
+                .status(404);
+        }
 
-    const searchDate: Date = new Date(searchDateNumber);
+        const searchDate: Date = new Date(searchDateNumber);
 
-    getNearEarthObjectsData(searchDate).then((nearEearthObjects) => {
-        console.log(`Returning ${nearEearthObjects.length} near earth objects`);
+        getNearEarthObjectsData(searchDate).then((nearEearthObjects) => {
+            console
+                .log(
+                    `Returning ${nearEearthObjects.length} near earth objects`
+                );
 
-        response.status(200).json({
-            date: dateString,
-            near_earth_objects: nearEearthObjects,
+            response.status(200).json({
+                date: dateString,
+                near_earth_objects: nearEearthObjects,
+            });
         });
-    });
+    }
 });
+// export const helloWorld = onRequest(
+//     {cors: ["*"]},
+//     (request, response) => {
+//
+//         console.log(request);
+//         logger.info("Hello logs!", {structuredData: true});
+//         response.status(200);
+//         response.json({
+//             message: "Firebase!",
+//         });
+//     });
+
+// export const getNearEarthObjects = onRequest((request, response) => {
+//
+// });
